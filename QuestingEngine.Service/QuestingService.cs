6d3e48ffa086cf200e5c6ec7a0318df084d652c1
Thinh Truong <thinh.truong@smartaction.com>
2021-12-06 01:35:28 +0700

    add questing service

---
+++ QuestingEngine.Service/QuestingService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace QuestingEngine.Service
+{
+    public interface IQuestingService
+    {
+        Task<int> CalculateQuestPointEarned(int chipAmountBet, int playerLevel);
+    }
+
+    public class QuestingService : IQuestingService
+    {
+        public Task<int> CalculateQuestPointEarned(int chipAmountBet, int playerLevel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
