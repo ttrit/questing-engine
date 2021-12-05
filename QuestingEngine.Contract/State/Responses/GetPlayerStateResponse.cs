10b50e9942c10808880a862434df9b1411901ae5
Thinh Truong <thinh.truong@smartaction.com>
2021-12-06 00:39:15 +0700

    add endpoint for state api

---
+++ QuestingEngine.Contract/State/Responses/GetPlayerStateResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace QuestingEngine.Contract.State.Responses
+{
+    public class GetPlayerStateResponse
+    {
+        public int TotalQuestPercentCompleted { get; set; }
+        public int LastMilestoneIndexCompleted { get; set; }
+    }
+}
---
+++ QuestingEngine/Controllers/StateController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuestingEngine.Contract.State.Responses;
+using System;
+using System.Threading.Tasks;
+
+namespace QuestingEngine.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StateController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<GetPlayerStateResponse> GetPlayerState(string playerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
