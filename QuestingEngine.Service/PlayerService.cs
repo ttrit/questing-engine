262848992ce1b8aa5580b26c6a43930f2f4b0b51
Thinh Truong <thinh.truong@smartaction.com>
2021-12-08 01:52:31 +0700

    add player service

---
+++ QuestingEngine.Service/PlayerService.cs
@@ -0,0 +1,35 @@
+﻿using QuestingEngine.Repository;
+using System;
+using System.Threading.Tasks;
+
+namespace QuestingEngine.Service
+{
+    public interface IPlayerService
+    {
+        Task GetCurrentQuestStatus(string playerId);
+    }
+
+    public class PlayerService : IPlayerService
+    {
+        private readonly IPlayerRepository _playerRepository;
+        private readonly IMilestoneRepository _milestoneRepository;
+        private readonly IQuestRepository _questRepository;
+
+        public PlayerService(IPlayerRepository playerRepository,
+            IMilestoneRepository milestoneRepository,
+            IQuestRepository questRepository)
+        {
+            _playerRepository = playerRepository;
+            _milestoneRepository = milestoneRepository;
+            _questRepository = questRepository;
+        }
+
+        public async Task GetCurrentQuestStatus(string playerId)
+        {
+            var player = await _playerRepository.GetAsync(playerId);
+            var currentQuest = await _questRepository.GetAsync(player.CurrentQuest.ToString());
+
+
+        }
+    }
+}
--- QuestingEngine/Controllers/ProgressController.cs
+++ QuestingEngine/Controllers/ProgressController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QuestingEngine.Contract.Progress.Requests;
 using QuestingEngine.Contract.Progress.Responses;
+using QuestingEngine.Service;
 using System;
 using System.Threading.Tasks;
 
@@ -10,10 +11,21 @@ namespace QuestingEngine.Controllers
     [ApiController]
     public class ProgressController : ControllerBase
     {
+        private readonly IQuestingService _questingService;
+
+        public ProgressController(IQuestingService questingService)
+        {
+            _questingService = questingService;
+        }
+
         [HttpPost]
         public async Task<CreateMilestoneResponse> CreateMilestone(CreateMilestoneRequest request)
         {
-            throw new NotImplementedException();
+            var response = new CreateMilestoneResponse();
+
+            response.QuestPointsEarned = await _questingService.CalculateQuestPointEarned(request.ChipAmountBet, request.PlayerLevel);
+
+
         }
     }
 }
